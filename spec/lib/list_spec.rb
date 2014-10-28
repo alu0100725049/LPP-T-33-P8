efb8c8f7984e0fc7458f9b26acd4dcfb713346da
David Fernando Redondo Durand <alu0100851700@ull.edu.es>
2014-10-28 13:06:36 +0000

    Pequeños cambios en las pruebas

--- spec/lib/list_spec.rb
+++ spec/lib/list_spec.rb
@@ -5,46 +5,39 @@ describe List do
 	it "Se extrae el primer elemento de la lista" do
 		@l = List.new(:value => 'Dato1 ')
 
-		expect(@l.elm(0).dato) == 'Dato1 '
+		expect(@l.elm(0).value) == 'Dato1 '
 	end
 
 	it "Se puede insertar un elemento" do
-		@l = List.new(:value => 'Dato1 ',
-				:next => nil))
+		@l = List.new(:value => 'Dato1 ')
 		@n2 = Node.new(	
-				:value => 'Dato2 ',
-				:next => nil)
+				:value => 'Dato2 ')
 
 		@l.push(:elm => [@n2])
 
-		expect(@l.elm(1).dato) == 'Dato2 '
+		expect(@l.elm(1).value) == 'Dato2 '
 	end
 
 	it "Se pueden insertar varios elementos" do
-		@l = List.new(:value => 'Dato1 ',
-				:next => nil))
+		@l = List.new(:value => 'Dato1 ')
 		@n2 = Node.new(	
-				:value => 'Dato2 ',
-				:next => nil)
+				:value => 'Dato2 ')
 		@n3 = Node.new(	
-				:value => 'Dato3 ',
-				:next => nil)
+				:value => 'Dato3 ')
 
 		@l.push(:elm => [@n2, @n3])
 
-		expect(@l.elm(1).dato) == 'Dato2 '
-		expect(@l.elm(2).dato) == 'Dato3 '
+		expect(@l.item(1).value) == 'Dato2 '
+		expect(@l.item(2).value) == 'Dato3 '
 	end
 
 	it "Debe existir una lista con su cabeza" do
 		@l = List.new(:value => 'Dato1 ',
 				:next => nil))
 		@n2 = Node.new(	
-				:value => 'Dato2 ',
-				:next => nil)
+				:value => 'Dato2 ')
 		@n3 = Node.new(	
-				:value => 'Dato3 ',
-				:next => nil)
+				:value => 'Dato3 ')
 
 		@l.push(:elm => [@n2, @n3])
 
--- spec/lib/node_spec.rb
+++ spec/lib/node_spec.rb
@@ -4,10 +4,13 @@ require "node"
 describe Node do 
 	it "Debe existir un Nodo de la lista con sus datos y su siguiente" do
 		@n1 = Node.new(
-				:value => 'Dato1 ',
-				:next => @n2)
+				:value => 'Dato1 ')
+		@n2 = Node.new(
+				:value => 'Dato2 ')
 
-		expect(@n.value) == 'Dato1 '
-		expect(@n.next) == @n2 
+		@n1.next = @n2
+
+		expect(@n1.value) == 'Dato1 '
+		expect(@n1.next) == @n2 
 	end
 end
