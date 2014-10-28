8e04e7ef48086fd44d40e7101f063cefbff545e0
David Fernando Redondo Durand <alu0100851700@ull.edu.es>
2014-10-28 12:41:56 +0000

    Añadidas todas las pruebas

--- spec/lib/list_spec.rb
+++ spec/lib/list_spec.rb
@@ -2,5 +2,52 @@ require "spec_helper"
 require "list"
 
 describe List do 
+	it "Se extrae el primer elemento de la lista" do
+		@l = List.new(:value => 'Dato1 ')
 
+		expect(@l.elm(0).dato) == 'Dato1 '
+	end
+
+	it "Se puede insertar un elemento" do
+		@l = List.new(:value => 'Dato1 ',
+				:next => nil))
+		@n2 = Node.new(	
+				:value => 'Dato2 ',
+				:next => nil)
+
+		@l.push(:elm => [@n2])
+
+		expect(@l.elm(1).dato) == 'Dato2 '
+	end
+
+	it "Se pueden insertar varios elementos" do
+		@l = List.new(:value => 'Dato1 ',
+				:next => nil))
+		@n2 = Node.new(	
+				:value => 'Dato2 ',
+				:next => nil)
+		@n3 = Node.new(	
+				:value => 'Dato3 ',
+				:next => nil)
+
+		@l.push(:elm => [@n2, @n3])
+
+		expect(@l.elm(1).dato) == 'Dato2 '
+		expect(@l.elm(2).dato) == 'Dato3 '
+	end
+
+	it "Debe existir una lista con su cabeza" do
+		@l = List.new(:value => 'Dato1 ',
+				:next => nil))
+		@n2 = Node.new(	
+				:value => 'Dato2 ',
+				:next => nil)
+		@n3 = Node.new(	
+				:value => 'Dato3 ',
+				:next => nil)
+
+		@l.push(:elm => [@n2, @n3])
+
+		expect(@l.head.dato) == 'Dato2 '
+	end
 end
--- spec/lib/node_spec.rb
+++ spec/lib/node_spec.rb
@@ -2,5 +2,12 @@ require "spec_helper"
 require "node"
 
 describe Node do 
+	it "Debe existir un Nodo de la lista con sus datos y su siguiente" do
+		@n1 = Node.new(
+				:value => 'Dato1 ',
+				:next => @n2)
 
+		expect(@n.value) == 'Dato1 '
+		expect(@n.next) == @n2 
+	end
 end
