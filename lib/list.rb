d3cba6c6aa9f0e787a0dda43d3614c4b3a438b26
CORREA BARRETO <alu4379@cc128.(none)>
2014-10-30 18:59:29 +0000

    Diseño clases list.rb y node.rb con algunos errores

--- lib/list.rb
+++ lib/list.rb
@@ -1,6 +1,32 @@
 require 'list/version'
 
 class List
+
+	attr_accessor :elm
+
+	def initialize(args)
+		@elm = args [:elm]
+	end
+
+	def show
+		puts "Elementos: #{@elm}"
+	end
+
+	def push(args)
+		elementos = @elm
+		elementos << args
+	end
+
+	def pop
+		elementos = @elm
+		elementos.shift
+	end
+
+	def head
+		elementos = @elm
+		cabecera = elementos(0)
+		puts "Primer elemento: #{cabecera}"
+	end
 end
 
 if __FILE__ == $0	#Se ejecuta si el programa se lanza desde el fichero
--- lib/node.rb
+++ lib/node.rb
@@ -2,6 +2,15 @@ require 'node/version'
 
 class Node
 
+Node = Struct.new(:value, :next)
+
+	attr_accessor :value, :next
+
+	def initialize(args)
+		@value = args[:value]
+		@next = args [:next]
+	end
+
 end
 
 if __FILE__ == $0	#Se ejecuta si el programa se lanza desde el fichero
